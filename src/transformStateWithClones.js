aaad55a67016198c20315ffc5e1e30eb501eec65
Mykhailo Doloh <dologmihajlo31@gmail.com>
2025-08-08 22:16:38 +0300

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,41 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let current = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        current = {};
+        break;
+
+      case 'addProperties': {
+        const extra = action.extraData || {};
+
+        current = { ...current, ...extra };
+        break;
+      }
+
+      case 'removeProperties': {
+        const keys = action.keysToRemove || [];
+        const next = { ...current };
+        // eslint-disable-next-line padding-line-between-statements
+        for (const k of keys) {
+          delete next[k];
+        }
+        current = next;
+        break;
+      }
+
+      default:
+        current = { ...current };
+    }
+
+    result.push({ ...current });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
